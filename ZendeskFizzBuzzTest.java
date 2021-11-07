01ab26d3c1c257d7131415bf2a0dd1e4b3088b46
michaelPro89 <47365331+michaelPro89@users.noreply.github.com>
2021-11-07 14:13:53 +0100

    Update ZendeskFizzBuzzTest.java

--- ZendeskFizzBuzzTest.java
+++ ZendeskFizzBuzzTest.java
@@ -1,9 +1,12 @@
 package zendesk.fizzbizz.test;
-
 /**
- *
- * @author Michal Switala
- */
+* # FizzBuzz
+* Have you ever wondered how to pass famous FizzBuzz programming test on your interview ?
+* Here is a simple solution for that if you need it.
+*
+* @author Michal Switala
+* You can reach me out on : https://github.com/michaelPro89/
+*/
 public class ZendeskFizzBuzzTest {
     
     
